399433b776aa2b959a5b09e6bdcce2293d2c4bfd
Rizky <rizkyterm@gmail.com>
2023-06-11 13:48:45 +0700

    Initial commit

---
+++ home.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+if (!isset($_SESSION["user"])) {
+   header("Location: index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QUIZZ</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <!-- logo unimed -->
+    <a href="https://www.unimed.ac.id">
+        <div class="container-logo">
+            <img src="assets/unimed.png" alt="" class="logo-img" >
+        </div>
+    </a>
+    <!-- logo unimed end -->
+    <!-- main menu -->
+    <!-- sun -->
+    <!-- <img src="assets/sun.gif" alt="" class="img-sun"> -->
+    <!-- sun -->
+    <div class="container-home position-absolute top-0 end-0">
+        <div class="home d-grid gap-2 col-3 mx-auto">
+            <a class="btn my-4 btn-lg" type="button" href="materi.html">Materi</a>
+            <a class="btn my-4 btn-lg" type="button" href="quizz.html">Quizz</a>
+        </div>
+    </div>
+
+    <a class="btn btn-logout" type="button" href="logout.php">logout</a>
+    <!-- audio -->
+    <audio autoplay loop>
+        <source src="assets/bcksnd.mp3" type="audio/mp3">
+    </audio>
+    <!-- audio end -->
+    <!-- end main menu -->
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Perintah dari keyboard untuk melakukan cetak miring pada tulisan di Microsoft Word adalah...",
+    answer: "CTRL + I",
+    options: [
+      "CTRL + I",
+      "CTRL + A",
+      "CTRL + U",
+      "CTRL + C"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Tab yang digunakan untuk mengatur tampilan halaman dokumen adalah ....",
+    answer: "View",
+    options: [
+      "Home",
+      "Insert",
+      "View",
+      "Reference"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Untuk melakukan Justify suatu teks atau paragraf ditunjukkan ikon nomor .....",
+    answer: "4",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Format ukuran kertas 210 x 297 mm adalah ......",
+    answer: "A4",
+    options: [
+      "A4",
+      "A5",
+      "Letter",
+      "Legal"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Urutan perintah yang digunakan untuk menyisipkan gambar / foto di dokumen Word adalah : ......",
+    answer: "Insert - Picture",
+    options: [
+      "AInsert - Chart",
+      "Insert - Smart Art",
+      "Insert - Shapes",
+      "Insert - Picture"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Cara membuat file atau dokumen baru pada program Microsoft Word 2007 adalah …",
+    answer: "Office Button - New - Blank Document",
+    options: [
+      "Office Button - New - Blank Document",
+      "Office Button - New - Open",
+      "Home - New - Blank Document",
+      "Insert - New - Blank Document"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Program yang sama dengan word adalah…..",
+    answer: "Lotus 123",
+    options: [
+      "Chiwriter",
+      "Lotus 123",
+      "Quattro Pro",
+      "Mac"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Cara membuat sebuah tabel dengan garis tebal adalah…",
+    answer: "Format-Font-Table",
+    options: [
+      "Format-Font-Table",
+      "Format-Border",
+      "Format-Fill",
+      "Format-Allignment"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Shortcut Keys untuk masuk menu dalam MS Word Help adalah….",
+    answer: "F1",
+    options: [
+      "ALT-F1",
+      "CTRL+ATL+F1",
+      "F1",
+      "CTRL+F1"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Shortcut CTRL+E di dalam MS Word digunakan untuk….",
+    answer: "Paragraf Center",
+    options: [
+      "Masuk ke menu edit",
+      "Export File",
+      "Paragraf Center",
+      "Exit"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
